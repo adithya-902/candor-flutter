1d80a682080181cd7a0c788fec1cbd6fde25c438
adithya-902 <adithyabiju902@gmail.com>
2023-08-31 22:14:42 +0530

    added utils

---
+++ candor_flutter/lib/screens/signIn.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class signIn extends StatelessWidget {
+  const signIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
